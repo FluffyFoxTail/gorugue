f678e9ab92dc1c16b563916bf7e1beac8dfbf172
FluffyFoxTail <fluffyfoxtail988@gmail.com>
2024-07-17 23:37:51 +0300

    Add room generation functions

--- game/gamemap/level/level.go
+++ game/gamemap/level/level.go
@@ -6,11 +6,13 @@ import (
 	"github.com/FluffyFoxTail/gorogue/game/gamemap/level/maptile"
 	"github.com/FluffyFoxTail/gorogue/game/gamemap/level/room"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"log"
 )
 
 const (
 	MIN_ROOM_SIZE   = 6
-	MAX__ROOM_SIZE  = 10
+	MAX_ROOM_SIZE   = 10
 	MAX_ROOMS_COUNT = 30
 )
 
@@ -42,17 +44,19 @@ func (l *Level) DrawLevel(gd *gamedata.GameData, screen *ebiten.Image) {
 
 // GenerateLevelTiles creates a new Dungeon Level Map.
 func (l *Level) GenerateLevelTiles(gd *gamedata.GameData) {
-	tiles := l.CreateTiles(gd)
+	tiles := l.InitTiles(gd)
 	l.Tiles = tiles
+	isContainsRoom := false
 
 	for idx := 0; idx < MAX_ROOMS_COUNT; idx++ {
-		w := dice.GetRandomBetween(MIN_ROOM_SIZE, MAX__ROOM_SIZE)
-		h := dice.GetRandomBetween(MIN_ROOM_SIZE, MAX__ROOM_SIZE)
-		x := dice.GetDiceRoll(gd.ScreenWidth-w-1) - 1
-		y := dice.GetDiceRoll(gd.ScreenHeight-h-1) - 1
+		w := dice.GetRandomBetween(MIN_ROOM_SIZE, MAX_ROOM_SIZE)
+		h := dice.GetRandomBetween(MIN_ROOM_SIZE, MAX_ROOM_SIZE)
+		x := dice.GetDiceRoll(gd.ScreenWidth - w - 1)
+		y := dice.GetDiceRoll(gd.ScreenHeight - h - 1)
 
 		newRoom := room.NewRectangle(x, y, w, h)
 		isCanAddOnMap := true
+
 		for _, otherRoom := range l.Rooms {
 			if newRoom.IsIntersect(otherRoom) {
 				isCanAddOnMap = false
@@ -62,12 +66,27 @@ func (l *Level) GenerateLevelTiles(gd *gamedata.GameData) {
 
 		if isCanAddOnMap {
 			l.createRoom(newRoom, gd)
+			if isContainsRoom {
+				nexX, nexY := newRoom.Center()
+				prevX, prevY := l.Rooms[len(l.Rooms)-1].Center()
+
+				coinFlip := dice.GetDiceRoll(2)
+
+				if coinFlip == 2 {
+					l.createHorizontalTunnel(prevX, nexX, prevY, gd)
+					l.createVerticalTunnel(prevY, nexY, prevX, gd)
+				} else {
+					l.createHorizontalTunnel(prevX, nexX, nexY, gd)
+					l.createVerticalTunnel(prevY, nexY, prevX, gd)
+				}
+			}
 			l.Rooms = append(l.Rooms, newRoom)
+			isContainsRoom = true
 		}
 	}
 }
 
-func (l *Level) CreateTiles(gd *gamedata.GameData) []*maptile.MapTile {
+func (l *Level) InitTiles(gd *gamedata.GameData) []*maptile.MapTile {
 	tiles := make([]*maptile.MapTile, gd.ScreenHeight*gd.ScreenWidth)
 	index := 0
 
@@ -86,8 +105,39 @@ func (l *Level) createRoom(room *room.Rectangle, gd *gamedata.GameData) {
 		for x := room.X1 + 1; x < room.X2; x++ {
 			index := l.GetIndexFromXY(x, y, gd)
 			l.Tiles[index].Blocked = false
-			floor := maptile.NewMapTale(maptile.FLOOR, x, y, false)
-			l.Tiles[index] = floor
+			floor, _, err := ebitenutil.NewImageFromFile("assets/floor32.png")
+			if err != nil {
+				log.Fatal(err)
+			}
+			l.Tiles[index].Image = floor
+		}
+	}
+}
+
+func (l *Level) createHorizontalTunnel(x1, x2, y int, gd *gamedata.GameData) {
+	for x := min(x1, x2); x < max(x1, x2)+1; x++ {
+		index := l.GetIndexFromXY(x, y, gd)
+		if index > 0 && index < gd.ScreenWidth*gd.ScreenHeight {
+			l.Tiles[index].Blocked = false
+			floor, _, err := ebitenutil.NewImageFromFile("assets/hallway32.png")
+			if err != nil {
+				log.Fatal(err)
+			}
+			l.Tiles[index].Image = floor
+		}
+	}
+}
+
+func (l *Level) createVerticalTunnel(y1, y2, x int, gd *gamedata.GameData) {
+	for y := min(y1, y2); y < max(y1, y2)+1; y++ {
+		index := l.GetIndexFromXY(x, y, gd)
+		if index > 0 && index < gd.ScreenWidth*gd.ScreenHeight {
+			l.Tiles[index].Blocked = false
+			floor, _, err := ebitenutil.NewImageFromFile("assets/hallway32.png")
+			if err != nil {
+				log.Fatal(err)
+			}
+			l.Tiles[index].Image = floor
 		}
 	}
 }
